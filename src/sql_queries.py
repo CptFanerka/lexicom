71e0123806f07e41d9178baa998990d16ff9a09d
Kseniia Lodiagina <lodyagina.ky@ibearsoft.com>
2023-04-11 15:56:22 +0300

    prettytable for tables

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -1,13 +1,28 @@
 import sqlite3
 import random
 import time
+from prettytable import PrettyTable
+
+
+def print_table(cursor, table_name):
+    query = f'SELECT * FROM {table_name} LIMIT 5'
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    table = PrettyTable()
+    table.field_names = [i[0] for i in cursor.description]
+    for row in rows:
+        table.add_row(row)
+
+    print(table)
+
 
 db_connection = sqlite3.connect('lexicom_database.db')
 
 cursor = db_connection.cursor()
 
 cursor.execute('CREATE TABLE IF NOT EXISTS short_names (name TEXT, status INTEGER)')
-short_names_list = [(f'nazvanie{i}', random.randint(0, 1)) for i in range(1, 7)]
+short_names_list = [(f'nazvanie{i}', random.randint(0, 1)) for i in range(1, 700001)]
 cursor.executemany('INSERT INTO short_names VALUES (?, ?)', short_names_list)
 db_connection.commit()
 print('short_names table filled with data')
@@ -15,7 +30,7 @@ print('short_names table filled with data')
 cursor.execute('CREATE TABLE IF NOT EXISTS full_names (name TEXT, status INTEGER)')
 extensions = ['mp3', 'avi', 'mov', 'mkv', 'wav', 'jpg', 'png', 'gif']
 full_names_list = [(f'nazvanie{i}.{random.choice(extensions)}', None)
-                   for i in range(1, 5)]
+                   for i in range(1, 500001)]
 cursor.executemany('INSERT INTO full_names (name, status) VALUES (?, ?)', full_names_list)
 db_connection.commit()
 print('full_names table filled with data')
@@ -25,6 +40,9 @@ db_connection.execute('CREATE INDEX IF NOT EXISTS short_names_name_index ON shor
 db_connection.execute('CREATE INDEX IF NOT EXISTS full_names_name_index ON full_names (name)')
 db_connection.commit()
 print('indexes created')
+
+print_table(cursor=cursor, table_name='short_names')
+print_table(cursor=cursor, table_name='full_names')
 
 """
 EXAMPLE
@@ -50,5 +68,6 @@ db_connection.commit()
 end_time = time.time()
 
 print(f"time spent: {end_time - start_time} seconds")
+print_table(cursor=cursor, table_name='full_names')
 
 db_connection.close()
